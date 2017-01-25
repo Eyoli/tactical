ddcf41ab2bb095b2369412cbf1147b24f816c0cf
OBERT Clément <C.Obert@Viveris.lan>
2017-01-25 11:14:07 +0100

    pouet

--- tactical.js
+++ tactical.js
@@ -1,3 +1,33 @@
+function GraphicObject(x, y) {
+    this.x = x;
+    this.y = y;
+    
+    this.onCollisionWith(object) {
+        
+    };
+}
+
+function Rectangle(topLeft, topRight, bottomLeft, bottomRight) {
+    this.topLeft = topLeft;
+    this.topRight = topRight;
+    this.bottomLeft = bottomLeft;
+    this.bottomRight = bottomRight;
+}
+
+function Physics2d(area) {
+    this.area = area;
+    
+    this.objects = [];
+    
+    this.addObject = function(object) {
+        this.objects.add(object);
+    };
+    
+    this.detectCollisions = function(depth) {
+        
+    }
+}
+
 window.onload = function() {
 
     var pause = false;
